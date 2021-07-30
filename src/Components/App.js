3e2199ab1c52f21b74bcc3ba1f5e837678da9ebf
Sakshi Kumar <saks5011@gmail.com>
2021-07-30 15:32:03 +0530

    redux

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,23 @@
+import { data } from "../data";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => {
+            console.log("hello");
+            return <MovieCard movie={movie} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
